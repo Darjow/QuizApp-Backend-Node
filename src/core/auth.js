bb002e5087bfa302bc22758d82511de86f4e7f85
Dario Bronders <dario.bronders@student.hogent.be>
2022-08-04 21:59:20 +0200

    code cleanup: delete category/difficulty rest API + columns as they were unnecessary

--- src/core/auth.js
+++ src/core/auth.js
@@ -10,10 +10,8 @@ const requireAuthentication = async (ctx, next) => {
     ...session
   } = await userService.checkAndParseSession(authorization);
 
-  // Save the decoded session data in the current context's state
   ctx.state.session = session;
-  // Also save the JWT in case we e.g. need to perform a request in the name of the current user
-  ctx.state.authToken = authToken;
+    ctx.state.authToken = authToken;
 
   return next();
 }
